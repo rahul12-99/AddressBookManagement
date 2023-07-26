97b528b3e60832adaf3f01969727ca8e277817fb
rahul12-99/rahul12-99 <rahulkrrao162@gmail.com>
2023-07-26 06:03:49 +0530

    Added : Code for create a contact in addressbook

---
+++ Contact.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookManagement
+{
+    internal class Contact
+    {
+        public string firstName;
+        public string lastName;
+        public string address;
+        public string city;
+        public string state;
+        public string zipcode;
+        public string phoneNumber;
+        public string email;
+
+        public override string ToString()
+        {
+            return firstName.ToString() + " " + lastName.ToString() + " " + address.ToString() + " " +
+                address.ToString() + " " + city.ToString() + " " + state.ToString() + " " +
+                zipcode.ToString() + " " + phoneNumber.ToString() + " " + email.ToString();
+        }
+    }
+}
