9489125f7a9ffe9668e794a83e487c1011688aa4
rahul12-99/rahul12-99 <rahulkrrao162@gmail.com>
2023-07-26 06:00:55 +0530

    Added : Welcome message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AddressBookManagement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to AddressBook Management Systems");
+        }
+    }
+}
