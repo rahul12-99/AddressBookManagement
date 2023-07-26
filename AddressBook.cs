8609b3baf31c3c634c4fdee0f30111ee7fe67f06
rahul12-99/rahul12-99 <rahulkrrao162@gmail.com>
2023-07-26 06:16:21 +0530

    Added : Code for add new contact in addressbook

---
+++ AddressBook.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookManagement
+{
+    internal class AddressBook
+    {
+        ArrayList list = new ArrayList();
+        public Contact CreateContact()
+        {
+            Console.WriteLine("Plz enter contact details");
+            Contact contact = new Contact();
+            Console.WriteLine("Plz enter first name of contact");
+            contact.firstName = Console.ReadLine();
+            Console.WriteLine("Plz enter last name of contact");
+            contact.lastName = Console.ReadLine();
+            Console.WriteLine("Plz enter address of contact");
+            contact.address = Console.ReadLine();
+            Console.WriteLine("Plz enter city of contact");
+            contact.city = Console.ReadLine();
+            Console.WriteLine("Plz enter state of contact");
+            contact.state = Console.ReadLine();
+            Console.WriteLine("Plz enter zipCode of contact");
+            contact.zipcode = Console.ReadLine();
+            Console.WriteLine("Plz enter phoneNumber of contact");
+            contact.phoneNumber = Console.ReadLine();
+            Console.WriteLine("Plz enter email of contact");
+            contact.email = Console.ReadLine();
+            return contact;
+        }
+
+        public void AddContacts()
+        {
+            Console.WriteLine("Plz enter how many contacts you want to save");
+            int noOfContacts = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < noOfContacts; i++)
+            {
+                Contact contacts = CreateContact();
+                list.Add(contacts);
+            }
+        }
+
+        public void PrintContacts()
+        {
+            foreach (Contact contact in list)
+            {
+                Console.WriteLine(contact);
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,26 +4,10 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to AddressBook Management Systems");
-            Console.WriteLine("Plz enter contact details");
-            Contact contact = new Contact();
-            Console.WriteLine("Plz enter first name of contact");
-            contact.firstName = Console.ReadLine();
-            Console.WriteLine("Plz enter last name of contact");
-            contact.lastName = Console.ReadLine();
-            Console.WriteLine("Plz enter address of contact");
-            contact.address = Console.ReadLine();
-            Console.WriteLine("Plz enter city of contact");
-            contact.city = Console.ReadLine();
-            Console.WriteLine("Plz enter state of contact");
-            contact.state = Console.ReadLine();
-            Console.WriteLine("Plz enter zipCode of contact");
-            contact.zipcode = Console.ReadLine();
-            Console.WriteLine("Plz enter phoneNumber of contact");
-            contact.phoneNumber = Console.ReadLine();
-            Console.WriteLine("Plz enter email of contact");
-            contact.email = Console.ReadLine();
-            Console.WriteLine(contact.ToString());
+            Console.WriteLine("******* Welcome to addressBook Management System *******");
+            AddressBook addressBook = new AddressBook();
+            addressBook.AddContacts();
+            addressBook.PrintContacts();
         }
     }
 }
